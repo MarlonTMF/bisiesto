25e361ed56308f795e71df416cb5a9bb37532ac0
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-08-20 08:58:01 -0400

    Saludo por sexo(y nombre)

--- src/Saludar.js
+++ src/Saludar.js
@@ -1,4 +1,15 @@
-function saludador(nombre) {
-    return "Hola " + nombre;
+function saludador(nombre, genero) {
+
+  let saludo = "Hola ";
+
+    if(genero.toLowerCase() === "masculino"){
+      saludo += "Sr. ";
+    } else if(genero.toLowerCase() === "femenino"){
+      saludo += "Sra. "
+    }
+
+    saludo += nombre;
+    return saludo;
   }
+
   export default saludador;
--- src/presenter.js
+++ src/presenter.js
@@ -1,17 +1,21 @@
 import saludador from "./Saludar";
 
 const nombreInput = document.querySelector("#nombrePersona");
+const generoInput = document.querySelector("#sexoPersona");
 const form = document.querySelector("#nombre-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    const nombre = nombreInput.value;
-    const saludo = saludador(nombre);
+    const nombre = nombreInput.value.trim();
+    const genero = generoInput.value.trim().toLowerCase();
+
+    const saludo = saludador(nombre, genero);
 
     div.innerHTML = `<p>${saludo}</p>`;
 
+    nombreInput.disabled = true;
     nombreInput.disabled = true;
     event.submitter.disabled = true;
 });
